4196fbe166d067de79c74575138605bc05568909
cesargutierrez40 <cesargutierrez40@outlook.com>
2018-02-13 18:44:04 -0500

    ZombieQuestions

---
+++ ZombieQuestions.java
@@ -0,0 +1,172 @@
+import java.util.Scanner;
+
+public class ZombieQuestions{
+  
+  public static void main(String[] args){
+   
+    //constants are all capital.
+                                   //multiples words seperated by underscores
+   int numberOfStudents = 29;
+   double avgGPA = 3.78;
+   String name = "";
+   String weapon = "";
+   String companions = "";
+   
+    boolean gameOver = false; //or true
+   int age = 0;
+    
+    Scanner input = new Scanner (System.in);
+    
+    System.out.println("What is your name?");
+    
+    name = input.next();
+    
+    System.out.println("What is your melee weapon?");
+    
+    weapon = input.next();
+    
+    System.out.println("How many companions do you have?");
+    
+    companions = input.next();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  }
+}
