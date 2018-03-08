4d0e03e8f883c72350e876544eba6c4437a89da4
cesargutierrez40 <cesargutierrez40@outlook.com>
2018-03-08 18:38:36 -0500

    work

--- GameDriver.java
+++ GameDriver.java
@@ -4,12 +4,13 @@ Co Sci 290
 
 Hw Assignment
 */
-
+//command for importng Scanner from util. package
 import java.util.Scanner;
+
 
 public class GameDriver{
   
-  public static void main(String[] args){
+ public static void main(String[] args){
   
   Scanner input = new Scanner(System.in);
   int numberofFamily = 0;
@@ -17,11 +18,13 @@ public class GameDriver{
   int randomNum = 0;
   int minimum = 2;
   int maximum = 6;
-  
-
-
-  
-
+  String Yourname = "";
+  String Yourage = "";
+  String Yourgender = "";
+  String Yourjob = "";
+  String Yourlikes = "";
+  String Yourdislikes = "";
+   
     
   
     
@@ -31,8 +34,27 @@ public class GameDriver{
                        +"|    |   /    \\  / __ |/ __ \\__ \\   / __ |   |    | \\_  __ \\_/ __ \\__ \\   / __ | \n"
                        +"|    |  /   |  \\/ /_/\\  ___/ / __\\_/ /_/ |   |    `  \\  |\\/\\  ___/ / __\\_/ /_/ | \n"
                        +"|______/|___|  /\\____|\\___  >____  /\\____ |  /_______  /__|   \\___  >____ /\\____ | \n"
-                       +"    \\/      \\/    \\/     \\/      \\/          \\/            \\/     \\/      \\/   \n");
-    
+                        +"    \\/      \\/    \\/     \\/      \\/          \\/            \\/     \\/      \\/   \n");
+   
+   System.out.println("What is your name?");
+     Yourname = input.next();
+   
+   System.out.println("What is your age?");
+     Yourage = input.next();
+   
+   System.out.println("What is your gender?");
+     Yourgender = input.next();
+   
+   System.out.println("What is your job?");
+     Yourjob = input.next();
+   
+   System.out.println("What are your likes?");
+     Yourlikes = input.next();
+   
+   System.out.println("What are your dislikes?");
+     Yourdislikes = input.next();
+   
+   
    //Story Setup
    System.out.println("You turn on the news and see channels reporting on a disease spreading in your town.... \n");
    System.out.println("people are rising from the dead and eating the living..... \n");
@@ -40,15 +62,19 @@ public class GameDriver{
    System.out.println("you have to escape with all of your family members! \n");
    System.out.println("wait..... How many family members do you have again? \n");
    
+    //users input on number of family members
    numberofFamily = input.nextInt();
    
    System.out.println("\nYou gather all "  + numberofFamily +  " of your family memebers and prepare to leave." );
-   System.out.println("you realise that you may need a weapon.....");
+    
+   System.out.println("you realise that you may need a weapon but what?.....");
    
+    //users input on what weapon their using 
    Yourweapon = input.next();
    
    System.out.println("\nso you use the " + Yourweapon + " you had laying around the house and you prepare to fight the zombies.");
    
+   // random number generator to tell if the user succeeds or not
    randomNum = minimum + (int)(Math.random() * maximum);
    if(randomNum > 3){
      System.out.println("\nYou killed the invading zombies in your house.");
---
+++ Quiz3.java
@@ -0,0 +1,20 @@
+/*
+Cesar Gutierrez
+
+CoSci 290
+
+Group 1 Quiz 3
+*/
+import java.util.scanner
+
+public class Quiz3{
+
+public static void main(String [] args){
+
+Scanner input = new Scanner(System.in);
+
+
+
+}
+  
+}
