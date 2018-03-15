4a970d765a9eabab741be3684a4d8128f673da1f
cesargutierrez40 <cesargutierrez40@outlook.com>
2018-03-15 18:03:03 -0400

    quiz4

---
+++ Quiz4.java
@@ -0,0 +1,31 @@
+/*
+Cesar Gutierrez
+Co Sci 290
+
+Quiz4 Geometry area of a triangle
+
+*/
+
+import java.util.Scanner;
+
+public class Quiz4{
+
+public static void main(String[] args) {
+
+ Scanner input = new Scanner(System.in);
+ 
+ System.out.println("Enter the coordinates of three points seperated by spaces \n");
+ System.out.println("like x1 y1 x2 y2 x3 y3: \n");
+ 
+ double x1 = input.nextDouble();
+ double y1 = input.nextDouble();
+ double x2 = input.nextDouble();
+ double y2 = input.nextDouble();
+ double x3 = input.nextDouble();
+ double y3 = input.nextDouble();
+ 
+
+ 
+  
+ }
+}
