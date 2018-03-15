15be57359b35898b7ce278f88df58576e6d437a7
cesargutierrez40 <cesargutierrez40@outlook.com>
2018-03-15 18:41:58 -0400

    lab9

---
+++ Lab9.java
@@ -0,0 +1,24 @@
+/*
+Cesar Gutierrez
+Co Sci 290
+
+Lab9 reading an integer between 0 and 1000
+
+*/
+import java.util.Scanner;
+
+public class Lab9{
+
+public static void main(String[] args) {
+
+Scanner input = new Scanner(System.in);
+  
+  System.out.print("Enter a number between 0 and 1000: ");
+  
+  int number = input.nextInt();
+  
+  
+  
+  
+ }
+}
