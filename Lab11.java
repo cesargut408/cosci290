93b103eda1625d6d33498b8685e1313af6ce6d1f
cesargutierrez40 <cesargutierrez40@outlook.com>
2018-03-27 17:19:24 -0400

    Lab11 Midterm

--- Lab11.java
+++ Lab11.java
@@ -17,25 +17,30 @@ public class Lab11{
     Scanner input = new Scanner (System.in);
     
     //This identifies the variable that will be used for user input
-    int threedigit;
+    int fivedigit;
     
     //This prompts for a user input
-    System.out.println("Enter a three digit number:");
-    threedigit = input.nextInt();
+    System.out.println("Enter a fivedigit number:");
+    fivedigit = input.nextInt();
     
     //A series of modding that allows for all three digits to obtained
-    int digit3 = threedigit % 10;
-    int num = threedigit / 10;
-    int digit2 = num % 10;
-    int digit1 = num / 10; 
+    int digit5 = fivedigit % 10;
+    int num = fivedigit / 10;
+    int digit4 = num % 10;
+    int num2 = num / 10;
+    int digit3 = num2 % 10;
+    int num3 = num2 / 10;
+    int digit2 = num3 % 10;
+    int digit1 = num3 / 10;
     
-    //Using If to compare the vsriable for the first digit to the last one to give a output of it being palindome
-    if (digit1 == digit3)
-        System.out.println(threedigit + " is a palindrome");
+       //Using If to compare the vsriable for the first digit to the last one to give a output of it being palindome
+    if (digit5 == digit4 && digit1 == digit2)
+        System.out.println(fivedigit + " is a palindrome");
     
     //Using else to give a different output if the previous one isn't correct 
     else
-      System.out.println(threedigit + " is not a palindrome");
+      System.out.println(fivedigit + " is not a palindrome");
+ 
      
   }
 }
